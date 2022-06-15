8d969d0e506c7f156ab445ab67cfee5193b334d6
DarrenSanders <332030@student.mboutrecht.nl>
2022-06-15 20:55:09 +0200

    kmjhg

--- mvcframework/app/config/config.php
+++ mvcframework/app/config/config.php
@@ -1,15 +1,14 @@
 <?php
-    //Database params
-    define('DB_HOST', 'localhost'); //Add your db host
-    define('DB_USER', 'root'); // Add your DB root
-    define('DB_PASS', ''); //Add your DB pass
-    define('DB_NAME', 'mvcframework'); //Add your DB Name
+define("APP_ROOT", dirname(dirname(__FILE__)));
+//Database params
+const DB_HOST = 'localhost'; //Add your db host
+const DB_USER = 'root'; // Add your DB root
+const DB_PASS = ''; //Add your DB pass
+const DB_NAME = 'mvcframework'; //Add your DB Name
 
-    //APPROOT
-    define('APPROOT', dirname(dirname(__FILE__)));
+const URL_ROOT = 'https://mvcframework.nl/';
+const SITE_NAME = 'MVC Framework';
 
-    //URLROOT (Dynamic links)
-    define('URLROOT', 'http://localhost/mvcframework');
+//APPROOT
 
-    //Sitename
-    define('SITENAME', 'MVC Framework');
+
