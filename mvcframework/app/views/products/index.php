0330d34b664bf5e1f328fe441f9a8ade9e8fbf3d
DarrenSanders <332030@student.mboutrecht.nl>
2022-06-17 12:11:59 +0200

    kmjhg

--- mvcframework/app/views/products/index.php
+++ mvcframework/app/views/products/index.php
@@ -1,5 +1,21 @@
-<a href="?page=<?php echo ($_GET['page'] ?? 1) - 1?>"><button <?php if (!isset($_GET['page']) or $_GET['page'] == 1) echo 'disabled' ?>>back</button></a>
-<a href="?page=<?php echo ($_GET['page'] ?? 1) + 1 ?>"><button>next</button></a>
+<?php
+$data = $data ?? array();
 
-<a href="products/create"><button>New product</button></a>
-<?php echo ($data['products']); ?>
+$page = $_GET['page'] ?? 1;
+$back = $page - 1;
+$next = $page + 1;
+
+$back_d = $page == 1 ? 'disabled' : '';
+?>
+
+    <a href="?page=<?= $back ?>">
+        <button <?= $back_d ?>>back</button>
+    </a>
+    <a href="?page=<?= $next ?>">
+        <button>next</button>
+    </a>
+
+    <a href="products/create">
+        <button>New product</button>
+    </a>
+<?php echo($data['products']); ?>
